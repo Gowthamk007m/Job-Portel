a1b2ba3a54bcd08e7ae34430325442418e0ddfc3
Gowtham K M <116723241+Gowthamk007m@users.noreply.github.com>
2024-06-06 10:33:16 +0530

    template restructured

--- jobPortal/core/views.py
+++ jobPortal/core/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_view(request):
-    return render(request,'home.html')
+    return render(request,'pages/home.html')
 
 def about_view(request):
-    return render(request, 'about.html')
+    return render(request, 'pages/about.html')
 
 def contact_view(request):
-    return render(request, 'contact.html')
+    return render(request, 'pages/contact.html')
